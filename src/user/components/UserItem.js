0c2217efbebec96f6471fa2277b7d55c010d2577
Lawrence <locaulker@gmail.com>
2020-08-21 03:41:22 -0400

    added UsersList component

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './UserItem.scss'
+
+const UserItem = props => {
+  return <div>User Item</div>
+}
+
+export default UserItem
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList'
+
 const Users = () => {
-  return <h2>User's works!</h2>
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Max Schwarz',
+      image: 'https://randomuser.me/api/portraits/women/62.jpg',
+      places: 3
+    }
+  ]
+
+  return <UsersList items={USERS} />
 }
 
 export default Users
