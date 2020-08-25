40a56df0dc74fb4fafe67d78d1c983d5a962f190
Lawrence <Caulker>
2020-08-24 22:21:14 -0400

    added and styled form input. Updated googlemaps to correct error

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -9,27 +9,27 @@ const DUMMY_PLACES = [
     title: 'Empire State Building',
     description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: '20 W 34th St, New York, NY 10001',
+      'https://images.unsplash.com/photo-1543969082-403d62815e2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
+    address: '20 W 34th St, NY, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584
+      lng: -73.9878584,
     },
-    creator: 'u1'
+    creator: 'u1',
   },
   {
     id: 'p2',
     title: 'Empire State Building',
     description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: '20 W 34th St, New York, NY 10001',
+      'https://cdn.pixabay.com/photo/2014/01/30/18/26/singapore-river-255116_1280.jpg',
+    address: '20 W 34th St, NY, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584
+      lng: -73.9878584,
     },
-    creator: 'u2'
-  }
+    creator: 'u2',
+  },
 ]
 
 const UserPlaces = () => {
